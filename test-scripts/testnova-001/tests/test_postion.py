3a49dbbf9e2f0f3c37c697eed0f88a921057cfcf
Tharun <101982719+Tharun1110@users.noreply.github.com>
2026-06-18 17:13:18 +0530

    Update pytest framework: testnova-001 - add tests/test_postion.py

---
+++ test-scripts/testnova-001/tests/test_postion.py
@@ -0,0 +1,12 @@
+import pytest
+from pages.postion_page import PostionPage
+
+def test_postion(page, screenshot_dir, base_url, test_username, test_password):
+    test_page = PostionPage(page, screenshot_dir)
+    test_page.navigate(base_url)
+    test_page.fill_email(test_username)
+    test_page.fill_password(test_password)
+    test_page.click_login()
+    test_page.click_positions_link()
+    test_page.fill_global_search("software Trainee")
+    test_page.click_positions_button()
